9f758d149571b3c6d328a6bfc37b86233d32807f
PrakashSingh65 <pratapsing5656@gmail.com>
2026-01-23 00:35:21 +0530

    first commit

---
+++ apna-college-clone/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+export default function App() {
+  return (
+    <div className='p-\[15px\] shadow-xl'>
+        <div className = 'max-w-\[1200px\] max-auto border-amber-600'>
+            <div className='w-\[100px\] border-red-600'>
+              <img src ="logo.png" className='w-full' alt="apna coll "/>
+            </div>
+        </div>
+    </div>
+  )
+}
