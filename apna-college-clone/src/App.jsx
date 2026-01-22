1ae7fff94fdd69f3f3fce2506dccd3b9320793ac
PrakashSingh65 <pratapsing5656@gmail.com>
2026-01-23 01:31:48 +0530

    Refactor layout and styling in App component for improved structure

--- apna-college-clone/src/App.jsx
+++ apna-college-clone/src/App.jsx
@@ -5,9 +5,13 @@ import React from 'react'
 export default function App() {
   return (
     <div className='p-\[15px\] shadow-xl'>
-        <div className = 'max-w-\[1200px\] max-auto border-amber-600'>
-            <div className='w-\[100px\] border-red-600'>
-              <img src ="/apna-college-icon.png" className='32 h-auto' alt="apna college "/>
+        <div className = 'max-w-\[1200px\] h-auto border-amber-600 flex items-center'>
+            <div className='w-\[100px\]'>
+              <img src ="/apna-college-icon.png" className='w-32 h-auto' alt="apna college "/>
+            </div>
+            <div className="">
+              <spam className='font-bold border border-b-\[3px\] border-[blue]'>Prime (AI/ML)</spam> New Sigma 11 New Courses Our Results DSA Sheet Log in
+
             </div>
         </div>
     </div>
